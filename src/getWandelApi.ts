fa5c6a457c4779688b7d14a61b153129492559f9
Stefan Wagner <stefan.wagner@wandelbots.com>
2024-09-19 15:38:49 +0200

    fix: add https://

--- src/getWandelApi.ts
+++ src/getWandelApi.ts
@@ -1,16 +1,21 @@
 import { NovaClient } from "@wandelbots/wandelbots-js"
 import { env } from "./runtimeEnv"
-import type { AxiosRequestConfig } from "axios"
 
 let nova: NovaClient | null = null
 
+const getSecureUrl = (url: string): string => {
+    return url.startsWith('https://') ? url : `https://${url}`;
+};
+
 export const getNovaClient = () => {
   if (!nova) {
+    const secureWandelAPIBaseURL = getSecureUrl(env.WANDELAPI_BASE_URL || "");
+    console.log(secureWandelAPIBaseURL)
     nova = new NovaClient({
       instanceUrl:
         typeof window !== "undefined"
-          ? new URL(env.WANDELAPI_BASE_URL || "", window.location.origin).href
-          : env.WANDELAPI_BASE_URL || "",
+          ? new URL(secureWandelAPIBaseURL || "", window.location.origin).href
+          : secureWandelAPIBaseURL || "",
       cellId: env.CELL_ID || "cell",
       username: env.NOVA_USERNAME || "",
       password: env.NOVA_PASSWORD || "",
